41ba2b46e94d3d8d622eaa29ab5f9b7dbecc6e80
Micah J Waldstein <mwaldstein@oxalis.io>
2026-01-27 12:23:44 -0500

    test: Expand test coverage for --min-value in context

--- tests/cli/context/walk.rs
+++ tests/cli/context/walk.rs
@@ -465,3 +465,361 @@ fn test_context_walk_semantic_inversion_disabled() {
         "Should include target note"
     );
 }
+
+#[test]
+fn test_context_walk_min_value_filter_all_match() {
+    let dir = tempdir().unwrap();
+
+    qipu()
+        .current_dir(dir.path())
+        .arg("init")
+        .assert()
+        .success();
+
+    let output1 = qipu()
+        .current_dir(dir.path())
+        .args(["create", "Root Note"])
+        .output()
+        .unwrap();
+    let id1 = extract_id(&output1);
+
+    let output2 = qipu()
+        .current_dir(dir.path())
+        .args(["create", "High Value Note"])
+        .output()
+        .unwrap();
+    let id2 = extract_id(&output2);
+
+    qipu()
+        .current_dir(dir.path())
+        .args(["value", "set", &id1, "90"])
+        .assert()
+        .success();
+
+    qipu()
+        .current_dir(dir.path())
+        .args(["value", "set", &id2, "95"])
+        .assert()
+        .success();
+
+    qipu()
+        .current_dir(dir.path())
+        .args(["link", "add", &id1, &id2, "--type", "related"])
+        .assert()
+        .success();
+
+    qipu()
+        .current_dir(dir.path())
+        .arg("index")
+        .assert()
+        .success();
+
+    qipu()
+        .current_dir(dir.path())
+        .args([
+            "context",
+            "--walk",
+            &id1,
+            "--walk-min-value",
+            "80",
+            "--related",
+            "0",
+        ])
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("Root Note"))
+        .stdout(predicate::str::contains("High Value Note"));
+}
+
+#[test]
+fn test_context_walk_min_value_filter_some_match() {
+    let dir = tempdir().unwrap();
+
+    qipu()
+        .current_dir(dir.path())
+        .arg("init")
+        .assert()
+        .success();
+
+    let output1 = qipu()
+        .current_dir(dir.path())
+        .args(["create", "Root Note"])
+        .output()
+        .unwrap();
+    let id1 = extract_id(&output1);
+
+    let output2 = qipu()
+        .current_dir(dir.path())
+        .args(["create", "Low Value Note"])
+        .output()
+        .unwrap();
+    let id2 = extract_id(&output2);
+
+    let output3 = qipu()
+        .current_dir(dir.path())
+        .args(["create", "High Value Note"])
+        .output()
+        .unwrap();
+    let id3 = extract_id(&output3);
+
+    qipu()
+        .current_dir(dir.path())
+        .args(["value", "set", &id1, "90"])
+        .assert()
+        .success();
+
+    qipu()
+        .current_dir(dir.path())
+        .args(["value", "set", &id2, "30"])
+        .assert()
+        .success();
+
+    qipu()
+        .current_dir(dir.path())
+        .args(["value", "set", &id3, "95"])
+        .assert()
+        .success();
+
+    qipu()
+        .current_dir(dir.path())
+        .args(["link", "add", &id1, &id2, "--type", "related"])
+        .assert()
+        .success();
+
+    qipu()
+        .current_dir(dir.path())
+        .args(["link", "add", &id1, &id3, "--type", "related"])
+        .assert()
+        .success();
+
+    qipu()
+        .current_dir(dir.path())
+        .arg("index")
+        .assert()
+        .success();
+
+    qipu()
+        .current_dir(dir.path())
+        .args([
+            "context",
+            "--walk",
+            &id1,
+            "--walk-min-value",
+            "80",
+            "--related",
+            "0",
+        ])
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("Root Note"))
+        .stdout(predicate::str::contains("High Value Note"))
+        .stdout(predicate::str::contains("Low Value Note").not());
+}
+
+#[test]
+fn test_context_walk_min_value_filter_with_defaults() {
+    let dir = tempdir().unwrap();
+
+    qipu()
+        .current_dir(dir.path())
+        .arg("init")
+        .assert()
+        .success();
+
+    let output1 = qipu()
+        .current_dir(dir.path())
+        .args(["create", "Root Note"])
+        .output()
+        .unwrap();
+    let id1 = extract_id(&output1);
+
+    let output2 = qipu()
+        .current_dir(dir.path())
+        .args(["create", "Default Value Note"])
+        .output()
+        .unwrap();
+    let id2 = extract_id(&output2);
+
+    qipu()
+        .current_dir(dir.path())
+        .args(["value", "set", &id1, "90"])
+        .assert()
+        .success();
+
+    qipu()
+        .current_dir(dir.path())
+        .args(["link", "add", &id1, &id2, "--type", "related"])
+        .assert()
+        .success();
+
+    qipu()
+        .current_dir(dir.path())
+        .arg("index")
+        .assert()
+        .success();
+
+    qipu()
+        .current_dir(dir.path())
+        .args([
+            "context",
+            "--walk",
+            &id1,
+            "--walk-min-value",
+            "50",
+            "--related",
+            "0",
+        ])
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("Root Note"))
+        .stdout(predicate::str::contains("Default Value Note"));
+}
+
+#[test]
+fn test_context_walk_min_value_filter_excludes_root() {
+    let dir = tempdir().unwrap();
+
+    qipu()
+        .current_dir(dir.path())
+        .arg("init")
+        .assert()
+        .success();
+
+    let output1 = qipu()
+        .current_dir(dir.path())
+        .args(["create", "Low Value Root"])
+        .output()
+        .unwrap();
+    let id1 = extract_id(&output1);
+
+    let output2 = qipu()
+        .current_dir(dir.path())
+        .args(["create", "High Value Child"])
+        .output()
+        .unwrap();
+    let id2 = extract_id(&output2);
+
+    qipu()
+        .current_dir(dir.path())
+        .args(["value", "set", &id1, "20"])
+        .assert()
+        .success();
+
+    qipu()
+        .current_dir(dir.path())
+        .args(["value", "set", &id2, "90"])
+        .assert()
+        .success();
+
+    qipu()
+        .current_dir(dir.path())
+        .args(["link", "add", &id1, &id2, "--type", "related"])
+        .assert()
+        .success();
+
+    qipu()
+        .current_dir(dir.path())
+        .arg("index")
+        .assert()
+        .success();
+
+    qipu()
+        .current_dir(dir.path())
+        .args([
+            "context",
+            "--walk",
+            &id1,
+            "--walk-min-value",
+            "80",
+            "--related",
+            "0",
+        ])
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("Low Value Root").not())
+        .stdout(predicate::str::contains("High Value Child").not());
+}
+
+#[test]
+fn test_context_walk_ignore_value_unweighted() {
+    let dir = tempdir().unwrap();
+
+    qipu()
+        .current_dir(dir.path())
+        .arg("init")
+        .assert()
+        .success();
+
+    let output1 = qipu()
+        .current_dir(dir.path())
+        .args(["create", "Root Note"])
+        .output()
+        .unwrap();
+    let id1 = extract_id(&output1);
+
+    let output2 = qipu()
+        .current_dir(dir.path())
+        .args(["create", "Low Value Note"])
+        .output()
+        .unwrap();
+    let id2 = extract_id(&output2);
+
+    let output3 = qipu()
+        .current_dir(dir.path())
+        .args(["create", "High Value Note"])
+        .output()
+        .unwrap();
+    let id3 = extract_id(&output3);
+
+    qipu()
+        .current_dir(dir.path())
+        .args(["value", "set", &id1, "50"])
+        .assert()
+        .success();
+
+    qipu()
+        .current_dir(dir.path())
+        .args(["value", "set", &id2, "0"])
+        .assert()
+        .success();
+
+    qipu()
+        .current_dir(dir.path())
+        .args(["value", "set", &id3, "100"])
+        .assert()
+        .success();
+
+    qipu()
+        .current_dir(dir.path())
+        .args(["link", "add", &id1, &id2, "--type", "related"])
+        .assert()
+        .success();
+
+    qipu()
+        .current_dir(dir.path())
+        .args(["link", "add", &id1, &id3, "--type", "related"])
+        .assert()
+        .success();
+
+    qipu()
+        .current_dir(dir.path())
+        .arg("index")
+        .assert()
+        .success();
+
+    qipu()
+        .current_dir(dir.path())
+        .args([
+            "context",
+            "--walk",
+            &id1,
+            "--walk-ignore-value",
+            "--related",
+            "0",
+        ])
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("Root Note"))
+        .stdout(predicate::str::contains("Low Value Note"))
+        .stdout(predicate::str::contains("High Value Note"));
+}

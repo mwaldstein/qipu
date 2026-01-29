6f1433f09bef50552e364593e171fa8cd22d31cf
Micah J Waldstein <mwaldstein@oxalis.io>
2026-01-28 21:42:07 -0500

    test: split tests/cli/search.rs: move records format tests to records_format.rs

--- tests/cli/search/basic.rs
+++ tests/cli/search/basic.rs
@@ -732,40 +732,6 @@ fn test_search_multi_word_and_semantics() {
 }
 
 #[test]
-fn test_search_records_format_truncated_field() {
-    let dir = tempdir().unwrap();
-
-    qipu()
-        .current_dir(dir.path())
-        .arg("init")
-        .assert()
-        .success();
-
-    qipu()
-        .current_dir(dir.path())
-        .args(["create", "Test Note"])
-        .assert()
-        .success();
-
-    let output = qipu()
-        .current_dir(dir.path())
-        .args(["--format", "records", "search", "test"])
-        .output()
-        .unwrap();
-
-    let stdout = String::from_utf8_lossy(&output.stdout);
-
-    assert!(
-        stdout.contains("H qipu=1 records=1"),
-        "search records output should have valid header"
-    );
-    assert!(
-        stdout.contains("mode=search"),
-        "search records output should contain mode=search"
-    );
-}
-
-#[test]
 fn test_search_field_weighting_impact() {
     // Test that field weights affect BM25 relevance scores
     let dir = tempdir().unwrap();
@@ -962,37 +928,5 @@ fn test_search_field_weighting_all_fields() {
     assert!(
         titles.contains(&"Tag Note"),
         "Tag match should be in results"
-    );
-}
-
-#[test]
-fn test_search_records_format_s_prefix() {
-    let dir = tempdir().unwrap();
-
-    qipu()
-        .current_dir(dir.path())
-        .arg("init")
-        .assert()
-        .success();
-
-    qipu()
-        .current_dir(dir.path())
-        .args(["create", "Test note with content"])
-        .assert()
-        .success();
-
-    let output = qipu()
-        .current_dir(dir.path())
-        .args(["--format", "records", "search", "content"])
-        .output()
-        .unwrap();
-
-    let stdout = String::from_utf8_lossy(&output.stdout);
-
-    // Note: search currently does not output S prefix (match_context is always None)
-    // This test documents the current behavior and can be updated if match_context is implemented
-    assert!(
-        !stdout.contains("S "),
-        "search records output currently does not contain S prefix"
     );
 }

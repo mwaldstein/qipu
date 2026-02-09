f5d11a5fbc2acf8f3d696d7439245b3739c8797c
Micah J Waldstein <mwaldstein@oxalis.io>
2026-02-09 00:28:43 -0500

    fix: Update Homebrew formula to v0.3.7

--- distribution/homebrew-qipu/Formula/qipu.rb
+++ distribution/homebrew-qipu/Formula/qipu.rb
@@ -1,8 +1,8 @@
 class Qipu < Formula
   desc "Zettelkasten-inspired knowledge management CLI"
   homepage "https://github.com/mwaldstein/qipu"
-  url "https://github.com/mwaldstein/qipu/archive/refs/tags/v0.3.6.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  url "https://github.com/mwaldstein/qipu/archive/refs/tags/v0.3.7.tar.gz"
+  sha256 "fa3e3ad00d761e0058f7ac4e8457999d9c139ddaf33401d7f9a6e1364e993936"
   license "MIT"
 
   depends_on "rust" => :build

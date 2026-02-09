b096ba3bf558657f085d3d7ad4c3e64735661a60
Micah J Waldstein <mwaldstein@oxalis.io>
2026-02-09 04:52:03 -0500

    fix: reduce sleep in test_list_filter_by_since to fix flaky timing

--- tests/cli/list/filtering_other.rs
+++ tests/cli/list/filtering_other.rs
@@ -110,11 +110,11 @@ fn test_list_filter_by_since() {
         .assert()
         .success();
 
-    std::thread::sleep(std::time::Duration::from_millis(250));
+    std::thread::sleep(std::time::Duration::from_millis(100));
 
     let since_time = chrono::Utc::now().to_rfc3339();
 
-    std::thread::sleep(std::time::Duration::from_millis(250));
+    std::thread::sleep(std::time::Duration::from_millis(100));
 
     qipu()
         .current_dir(dir.path())
